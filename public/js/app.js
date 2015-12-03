745c507157544269fff009fa6a22bf5d93707f73
alfonso <=>
2015-12-02 19:32:05 -0600

    alfonso

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,38 @@
 function init() {
-	
+	$("#btn-login").on('click', function login() {
+		$("#ModalLogin").removeClass('no-visibility').addClass('visibility');
+	});
+
+
+	$('#btn-auth').on('click', function auth(e) {
+		e.preventDefault();
+		var serialize = $("#form-auth").serializeObject();
+		var xhr = $.get('/api/auth/'+JSON.stringify(serialize));
+
+		xhr
+		.done(function (response, message_http, http) {
+			if(http.status == 200) {
+				
+			}
+			else {
+				alert("No existe el usuario");
+			}
+		})
+		.error(function (e) {
+			alert("No se encontró el usuario");
+		});
+
+	});
+
+
+
+	$("section.modal .close").on({
+		click: function click() {
+			$(this).parent().parent().removeClass('visibility').addClass('no-visibility');
+			$('body').css({overflow: 'auto'});
+			$("section.modal #container-form-service span").removeClass('field-error');
+		}
+	});
 };
 
 $(document).on('ready', init);
--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
+var router = express.Router();
+var cookieParser = require('cookie-parser');
+var connection = mongoose.createConnection("mongodb://chapin:chapin2015@ds061954.mongolab.com:61954/guatechapin");
 
+autoIncrement.initialize(connection);
+
+var userSchema = new Schema({
+	nombre: String,
+	password: String
+});
+
+userSchema.plugin(autoIncrement.plugin, 'User');
+var User = connection.model('User', userSchema);
+
+function createUser(user){
+	var newUser = new User(user);
+	return newUser;
+};
+
+function updateUserById(id, user) {
+	User.findOneAndUpdate({_id: id}, user, {}, function a() {});
+};
+
+
+app.use(cookieParser());
+app.use('/api', router);
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({
 	extended:true
@@ -12,5 +40,32 @@ app.use(bodyParser.json({}));
 app.get('/', function init(req, res) {
 	
 });
+
+
+//API REST
+router.get('/auth/:user', function auth(req, res) {
+	var user = JSON.parse(req.params.user);
+	
+	User.findOne(user, function (error, response) {
+		
+		if(error) {
+			res.status(500).json({
+				message: "No se encontró el usuario"
+			});
+		}
+		else {
+			if(!response) {
+				res.status(500).json({
+					message: "No se encontró el usuario"
+				});
+			}
+			else {
+				res.status(200).json({});
+			}
+			
+		}
+	});
+});
+
 
 app.listen(process.env.PORT || 5000);
