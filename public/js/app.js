d1213f78b7feb7c886f90686b3e85f4133c9fe4b
alfonso <=>
2015-12-03 12:49:54 -0600

    alfonso

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,70 @@
 function init() {
+
+	var xhr = $.get("/api/clothes");
+	xhr.done(function done(response, message_http, http) {
+		if(http.status == 200) {
+			
+			var categories = {
+				0: "tejidos",
+				1: "cueros",
+				2: "mostacilla",
+				3: "bolsos", 
+				4: "carteras",
+				5: "ropas" 
+			};
+
+
+
+			var clothes = response.clothes;
+			var offers = response.offers;
+
+			var html_offer = "<figure><img src='img/productos/"+ categories[offers[0].article.categoria] +"/"+offers[0].article.nombre+".jpg' class='image-responsive'> </figure><div id='description-gallery'> <h2 class='no-margin'>"+ offers[0].offer.porc_descuento +"%</h2> <h3 class='no-margin'>DESCUENTO <br /> NAVIDEÑO</h3> <div> <button>COMPRAR</button> </div> </div>";
+			$("#offer").html(html_offer);
+
+			var html_tejidos = [];
+			var html_cueros = [];
+			var html_mostacilla = [];
+			var html_bolsos = [];
+			var html_carteras = [];
+			var html_ropas = [];
+
+
+			clothes.tejidos.forEach(function each(cloth, index) {
+				html_tejidos.push("<article data-idproducto='"+ cloth._id +"'><figure><img src='img/productos/tejidos/"+ cloth.nombre +".jpg' > <figcaption> <div> <span>"+ cloth.descripcion +"</span> <button class='btn-add' data-idproducto='"+ cloth._id +"'>COMPRAR</button></div></figcaption></figure> </article>");				
+			});
+			clothes.cueros.forEach(function each(cloth, index) {
+				html_cueros.push("<article data-idproducto='"+ cloth._id +"'><figure><img src='img/productos/cueros/"+ cloth.nombre +".jpg' > <figcaption> <div> <span>"+ cloth.descripcion +"</span> <button class='btn-add' data-idproducto='"+ cloth._id +"'>COMPRAR</button></div></figcaption></figure> </article>");					
+			});
+
+			clothes.mostacilla.forEach(function each(cloth, index) {
+				html_mostacilla.push("<article data-idproducto='"+ cloth._id +"'><figure><img src='img/productos/mostacilla/"+ cloth.nombre +".jpg' > <figcaption> <div> <span>"+ cloth.descripcion +"</span> <button class='btn-add' data-idproducto='"+ cloth._id +"'>COMPRAR</button></div></figcaption></figure> </article>");					
+			});
+
+			clothes.bolsos.forEach(function each(cloth, index) {
+				html_bolsos.push("<article data-idproducto='"+ cloth._id +"'><figure><img src='img/productos/bolsos/"+ cloth.nombre +".jpg' > <figcaption> <div> <span>"+ cloth.descripcion +"</span> <button class='btn-add' data-idproducto='"+ cloth._id +"'>COMPRAR</button></div></figcaption></figure> </article>");				
+			});
+
+			clothes.carteras.forEach(function each(cloth, index) {
+				html_carteras.push("<article data-idproducto='"+ cloth._id +"'><figure><img src='img/productos/carteras/"+ cloth.nombre +".jpg' > <figcaption> <div> <span>"+ cloth.descripcion +"</span> <button class='btn-add' data-idproducto='"+ cloth._id +"'>COMPRAR</button></div></figcaption></figure> </article>");				
+			});
+
+			clothes.ropas.forEach(function each(cloth, index) {
+				html_ropas.push("<article data-idproducto='"+ cloth._id +"'><figure><img src='img/productos/ropas/"+ cloth.nombre +".jpg' > <figcaption> <div> <span>"+ cloth.descripcion +"</span> <button class='btn-add' data-idproducto='"+ cloth._id +"'>COMPRAR</button></div></figcaption></figure> </article>");				
+			});
+
+
+			$("#tejidos .articles").html(html_tejidos.join(""));
+			$("#cueros .articles").html(html_cueros.join(""));
+			$("#mostacilla .articles").html(html_mostacilla.join(""));
+			$("#bolsos .articles").html(html_mostacilla.join(""));
+			$("#carteras .articles").html(html_mostacilla.join(""));
+			$("#ropas .articles").html(html_mostacilla.join(""));
+		}
+		else {
+
+		}
+	});
+
 	$("#btn-login").on('click', function login() {
 		$("#ModalLogin").removeClass('no-visibility').addClass('visibility');
 	});
@@ -32,6 +98,12 @@ function init() {
 			$('body').css({overflow: 'auto'});
 			$("section.modal #container-form-service span").removeClass('field-error');
 		}
+	});
+
+	$(document).on('click', '.btn-add', function click(e) {
+		e.preventDefault();
+		var idproducto = $(this).attr('data-idproducto');
+		console.log(idproducto);		
 	});
 };
 
--- server.js
+++ server.js
@@ -15,8 +15,69 @@ var userSchema = new Schema({
 	password: String
 });
 
+var categorySchema = new Schema({
+	categoria: String
+});
+
+var personSchema = new Schema({
+	nombre: String,
+	apellido: String,
+	sexo: String,
+	fechanacimiento: Date,
+	email: String,
+	usuario: Number
+});
+
+/*
+
+*/
+
+var billSchema = new Schema({
+	total: Number,
+	fecha: Date,
+	cliente: Number,
+	tarjeta: Number
+});
+
+var detailBillSchema = new Schema({
+	factura: Number,
+	articulo: Number,
+	cantidad: Number,
+	precioVenta: Number
+});
+
+var articleSchema = new Schema({
+	nombre: String,
+	descripcion: String,
+	precio: Number,
+	existencia: Number,
+	categoria: Number
+});
+
+var offerSchema = new Schema({
+	articulo: Number,
+	porc_descuento: Number,
+	fecha_inicio: Date,
+	fecha_vencimiento: Date,
+	tipo: String
+});
+
 userSchema.plugin(autoIncrement.plugin, 'User');
+categorySchema.plugin(autoIncrement.plugin, 'Category');
+personSchema.plugin(autoIncrement.plugin, 'Person');
+billSchema.plugin(autoIncrement.plugin, 'Bill');
+detailBillSchema.plugin(autoIncrement.plugin, 'DetailBill');
+articleSchema.plugin(autoIncrement.plugin, 'Article');
+offerSchema.plugin(autoIncrement.plugin, 'Offer');
+
+
 var User = connection.model('User', userSchema);
+var Category = connection.model('Category', categorySchema);
+var Person = connection.model('Person', personSchema);
+var Bill = connection.model('Bill', billSchema);
+var DetailBill = connection.model('DetailBill', detailBillSchema);
+var Article = connection.model('Article', articleSchema);
+var Offer = connection.model('Offer', offerSchema);
 
 function createUser(user){
 	var newUser = new User(user);
@@ -67,5 +128,76 @@ router.get('/auth/:user', function auth(req, res) {
 	});
 });
 
+router.get('/clothes', function clothes(req, res) {
+	Article.find({}, function callback(error, clothes) {
+		if(error) {
+			res.status(404).json({
+				error: error
+			});
+		}
+		else {
+			Offer.find({}, function offer(error, offers) {
+				if(!error) {
+					
+					var offers_articles = [];
+
+					clothes.forEach(function each(cloth, index) {
+						for(var i = 0 ; i < offers.length ; i++) {
+							if(offers[i].articulo == cloth._id) {
+								offers_articles.push({article: cloth, offer: offers[i]});
+								clothes.splice(index, 1);
+							}
+						}
+					});
+					var articles = {
+						tejidos: [],
+						cueros: [],
+						mostacilla: [],
+						bolsos: [],
+						carteras: [],
+						ropas: []
+					};
+					clothes.forEach(function each(cloth, index) {
+						if(cloth.categoria == 0) { //Si es un tejido
+							if(articles.tejidos.length != 3) {
+								articles.tejidos.push(cloth);
+							}
+						}
+						else if(cloth.categoria == 1) { //Si es un cuero
+							if(articles.cueros.length != 3) {
+								articles.cueros.push(cloth);
+							}
+						}
+						else if(cloth.categoria == 2) { //si es un mostacilla
+							if(articles.mostacilla.length != 3) {
+								articles.mostacilla.push(cloth);
+							}
+						}
+						else if(cloth.categoria == 3) { //Si es un bolso
+							if(articles.bolsos.length != 3) {
+								articles.bolsos.push(cloth);
+							}
+						}
+						else if(cloth.categoria == 4) { //Si es un cartera
+							if(articles.carteras.length != 3) {
+								articles.carteras.push(cloth);
+							}
+						}
+						else if(cloth.categoria == 5) { //Si es una ropa
+							if(articles.ropas.length != 3) {
+								articles.ropas.push(cloth);
+							}
+						}
+					});
+
+					res.status(200).json({
+						clothes: articles,
+						offers: offers_articles
+					});
+				}
+			});
+		}
+	});
+});
 
 app.listen(process.env.PORT || 5000);
